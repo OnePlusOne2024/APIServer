c7d8a5e807400ac23fec01a18ad33817f7cfd993
ChyoPyoRo <allen15590@naver.com>
2024-04-05 20:54:55 +0900

    first commit

---
+++ src/test/java/org/spring/oneplusone/OneplusoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.oneplusone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneplusoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
