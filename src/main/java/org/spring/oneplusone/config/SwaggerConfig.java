9d298272038772246b72e656abd3bbd19bdb1d89
ChyoPyoRo <allen15590@naver.com>
2024-04-07 16:04:41 +0900

    Swagger 기본 세팅

---
+++ src/main/java/org/spring/oneplusone/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package org.spring.oneplusone.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("OnePlusOne-api-server")
+                        .version("0.1")
+                        .description("OnePlusOne api server의 API 명세서입니다."));
+    }
+}
